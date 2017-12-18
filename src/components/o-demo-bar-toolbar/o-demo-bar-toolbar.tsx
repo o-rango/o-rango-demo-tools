ff6796225a8252c36535e068500fcab692b0e164
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-18 14:18:58 +0100

    feat: update o-demo-bar-toolbar

--- src/components/o-demo-bar-toolbar/o-demo-bar-toolbar.tsx
+++ src/components/o-demo-bar-toolbar/o-demo-bar-toolbar.tsx
@@ -1,4 +1,5 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop,Element} from '@stencil/core';
+import {MDCToolbar} from '@material/toolbar';
 
 @Component({
   tag: 'o-demo-bar-toolbar',
@@ -7,13 +8,42 @@ import { Component, Prop } from '@stencil/core';
 })
 export class DemoToolbarComponent {
 
-  @Prop() first: string;
-  @Prop() last: string;
+  private toolbar:any;
+  @Element() el: HTMLElement;
+  @Prop() events: string;
+  @Prop() name: string;
+  @Prop() description: string;
+
+
+  componentDidLoad() {
+    const rootEl = this.el.querySelector('.mdc-toolbar');
+    this.toolbar = new MDCToolbar(rootEl);
+    this.toolbar.fixedAdjustElement = this.el.querySelector('.mdc-toolbar-fixed-adjust');
+  }
+
 
   render() {
     return (
-      <div>
-        Demo Toolbar
+      <div class="mdc-typography">
+        <header class="mdc-toolbar mdc-toolbar--fixed">
+          <div class="mdc-toolbar__row">
+            <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+              <h3 class="mdc-typography--subheading2">{this.name}</h3>
+            </section>
+            <section class="mdc-toolbar__section">
+            <o-demo-bar-select/>
+            </section>
+            <section class="mdc-toolbar__section mdc-toolbar__section--align-end">
+            <i class="material-icons">smartphone</i>
+            <i class="material-icons">laptop_chromebook</i>
+            </section>
+          </div>
+        </header>
+        <main class="mdc-toolbar-fixed-adjust">
+          <p class="demo-paragraph">
+            <slot/>
+          </p>
+        </main>
       </div>
     );
   }
