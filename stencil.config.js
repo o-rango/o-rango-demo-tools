1b8807f647bd30b0b53f70f2797a5a0120d64571
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-18 12:25:01 +0100

    chore : config stencil

---
+++ stencil.config.js
@@ -0,0 +1,17 @@
+exports.config = {
+  namespace: 'mycomponent',
+  generateDistribution: true,
+  bundles: [
+    { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select'] },
+    { components: ['o-demo-case'] }
+  ],
+  collections: [],
+  sassConfig: {
+    includePaths: ['node_modules']
+  }
+};
+
+exports.devServer = {
+  root: 'www',
+  watchGlob: '**/**'
+}
