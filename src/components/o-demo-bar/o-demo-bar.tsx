564457c616648d5763f90c97d91f9ea2ed4a3512
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-23 14:21:40 +0100

    feat : demo bar add styles and initial frame setup

--- src/components/o-demo-bar/o-demo-bar.tsx
+++ src/components/o-demo-bar/o-demo-bar.tsx
@@ -1,4 +1,5 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop , Element } from '@stencil/core';
+
 
 @Component({
   tag: 'o-demo-bar',
@@ -7,14 +8,36 @@ import { Component, Prop } from '@stencil/core';
 })
 export class DemoBarComponent {
 
+  private demoCases : any;
+  @Element() el: HTMLElement;
   @Prop() name: string;
   @Prop() last: string;
 
+  componentWillLoad() {
+    // Get inner demo cases props :
+    this.demoCases = this.el.querySelectorAll('o-demo-case');
+  };
+
+  componentDidLoad() {
+    const iframeContainer = this.el.shadowRoot.querySelector('#iframeContainer');
+    const iframe = document.createElement('iframe');
+    let html = this.demoCases[0].innerHTML;
+    html = html.replace(/=""/g, '').replace(/="{/g, '=\'{').replace(/}"/g, '}\'').replace(/="\[/g, '=\'\[').replace(/\]"/g, '\]\'').replace(/&quot;/g, '\"').replace(/='\[\[/g, '=\"\[\[').replace(/\]\]\'/g, '\]\]\"').replace(/='\{\{/g, '=\"\{\{').replace(/\}\}\'/g, '\}\}\"');
+    html = '<html><head></head><body style=`width=${window.innerWidth}; height:${window.innerHeight};` ontouchstart id="frameBody">' + html + '</body></html>';
+    iframeContainer.appendChild(iframe);
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(html);
+    iframe.contentWindow.document.close();
+  }
+
+
   render() {
     return (
-      <div>
+      <div id="demo-bar">
       <o-demo-bar-toolbar name={this.name}/>
-      <slot/>
+        <div id="frame-wrap">
+        <div id="iframeContainer"/>
+        </div>
       </div>
     );
   }
