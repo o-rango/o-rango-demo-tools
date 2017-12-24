689e36c7b108f0da41c95f070d7586ed10a1e7f0
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-24 02:27:18 +0100

    chore (config) update config for new components

--- stencil.config.js
+++ stencil.config.js
@@ -2,7 +2,7 @@ exports.config = {
   namespace: 'orango-dev-tools',
   generateDistribution: false,
   bundles: [
-    { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select'] },
+    { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select' , 'o-demo-bar-buttons'] },
     { components: ['o-demo-case'] }
   ],
   collections: [],
