cce9eec80f513f4bf69f6731c053e7d1b924acd4
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-24 02:25:32 +0100

    feat(refactor) : refactor demo bar and added prop to define background pattern

--- src/components/o-demo-bar/o-demo-bar.tsx
+++ src/components/o-demo-bar/o-demo-bar.tsx
@@ -1,5 +1,4 @@
-import { Component, Prop , Element } from '@stencil/core';
-
+import { Component, Prop, Element, Listen ,CssClassMap} from '@stencil/core';
 
 @Component({
   tag: 'o-demo-bar',
@@ -7,36 +6,77 @@ import { Component, Prop , Element } from '@stencil/core';
   shadow: true
 })
 export class DemoBarComponent {
-
-  private demoCases : any;
+  private demoCases: any;
+  private casesOptions: any;
+  private caseOptionSelected: number = 0;
   @Element() el: HTMLElement;
   @Prop() name: string;
-  @Prop() last: string;
+  @Prop() pattern: boolean = true;
 
   componentWillLoad() {
-    // Get inner demo cases props :
     this.demoCases = this.el.querySelectorAll('o-demo-case');
-  };
+    this.casesOptions = this._setSelect();
+  }
 
   componentDidLoad() {
-    const iframeContainer = this.el.shadowRoot.querySelector('#iframeContainer');
+    this._setIframe();
+  }
+
+
+  @Listen('toolbarButtonClicked')
+  toolbarButtonClickedHandler(event: CustomEvent) {
+    console.log('Click button : ', event.detail)
+    //TODO Handle click on toolbar buttons event.detail
+  }
+
+  @Listen('selectedCaseChanged')
+  selectedCaseChangedHandler(event: CustomEvent) {
+    this.caseOptionSelected = event.detail;
+    this._setIframe();
+  }
+
+  _setSelect() {
+    return Array.from(this.demoCases).map(function(item: any) {
+      return item.getAttribute('name');
+    });
+  }
+
+  _cleanIframe() {
+    const oldFrame = this.el.shadowRoot.querySelector('iframe');
+    if (oldFrame) {
+      oldFrame.remove();
+    }
+  }
+
+  _setIframe() {
+    this._cleanIframe();
+    const iframeContainer = this.el.shadowRoot.querySelector(
+      '#iframeContainer'
+    );
     const iframe = document.createElement('iframe');
-    let html = this.demoCases[0].innerHTML;
-    html = html.replace(/=""/g, '').replace(/="{/g, '=\'{').replace(/}"/g, '}\'').replace(/="\[/g, '=\'\[').replace(/\]"/g, '\]\'').replace(/&quot;/g, '\"').replace(/='\[\[/g, '=\"\[\[').replace(/\]\]\'/g, '\]\]\"').replace(/='\{\{/g, '=\"\{\{').replace(/\}\}\'/g, '\}\}\"');
-    html = '<html><head></head><body style=`width=${window.innerWidth}; height:${window.innerHeight};` ontouchstart id="frameBody">' + html + '</body></html>';
+    const frameH = Math.max(document.documentElement.clientHeight);
+    let html = this.demoCases[this.caseOptionSelected].innerHTML;
+    html = `<html><head></head><body ontouchstart id="frameBody">${html}</body></html>`;
+
+    iframe.height = (frameH - 85).toString();
     iframeContainer.appendChild(iframe);
     iframe.contentWindow.document.open();
     iframe.contentWindow.document.write(html);
     iframe.contentWindow.document.close();
   }
 
-
   render() {
+    const bgClasses : CssClassMap = {
+      'pattern' : this.pattern
+    }
     return (
       <div id="demo-bar">
-      <o-demo-bar-toolbar name={this.name}/>
-        <div id="frame-wrap">
-        <div id="iframeContainer"/>
+        <o-demo-bar-toolbar name={this.name}>
+          <o-demo-bar-select slot="center" options={this.casesOptions} />
+          <o-demo-bar-buttons slot="right" />
+        </o-demo-bar-toolbar>
+        <div id="frame-wrap" class={bgClasses}>
+          <div id="iframeContainer" />
         </div>
       </div>
     );
