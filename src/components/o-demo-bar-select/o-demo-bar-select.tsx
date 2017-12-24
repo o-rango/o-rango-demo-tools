d9173644354e9fc6e94f07c2c0a3a1be5694deb3
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-24 02:26:38 +0100

    feat() demo-select add events and options repeat

--- src/components/o-demo-bar-select/o-demo-bar-select.tsx
+++ src/components/o-demo-bar-select/o-demo-bar-select.tsx
@@ -1,48 +1,50 @@
-import { Component, Prop ,Element} from '@stencil/core';
-import {MDCSelect} from '@material/select';
+import { Component, Prop, Element, Event,EventEmitter} from '@stencil/core';
+import { MDCSelect } from '@material/select';
 @Component({
   tag: 'o-demo-bar-select',
   styleUrl: 'o-demo-bar-select.scss',
   shadow: false
 })
 export class DemoSelectComponent {
-
-  private select:any;
+  private select: any;
   @Element() el: HTMLElement;
+  @Event() selectedCaseChanged: EventEmitter;
+  selectedCaseChangedHandler(payload: any) {
+    this.selectedCaseChanged.emit(payload);
+  }
   @Prop() options: any;
 
   componentDidLoad() {
     const rootEl = this.el.querySelector('.mdc-select');
     this.select = new MDCSelect(rootEl);
-
-    this.select.listen('MDCSelect:change', (evt) => {
-          console.log(evt);
+    this.select.listen('MDCSelect:change', evt => {
+      this.selectedCaseChangedHandler(evt.detail.value);
     });
-   }
-   componentDidUnload() {
+    this.select.selectedIndex = 0;
+  }
+  componentDidUnload() {
     this.select.destroy();
   }
 
   render() {
     return (
       <div class="my-select-container">
-      <div class="mdc-select" role="listbox" tabindex="0">
-        <div class="mdc-select__surface">
-          <div class="mdc-select__label">Select demo case</div>
-          <div class="mdc-select__selected-text" />
-          <div class="mdc-select__bottom-line" />
+        <div class="mdc-select" role="listbox">
+          <div class="mdc-select__surface" tabindex="0">
+            <div class="mdc-select__label">Demo case</div>
+            <div class="mdc-select__selected-text" />
+            <div class="mdc-select__bottom-line" />
+          </div>
+          <div class="mdc-simple-menu mdc-select__menu">
+            <ul class="mdc-list mdc-simple-menu__items">
+            {this.options.map((option , index) => (
+                <li class="mdc-list-item" id={index} role="option" tabindex="0">
+                  {option}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div class="mdc-simple-menu mdc-select__menu">
-          <ul class="mdc-list mdc-simple-menu__items">
-            <li class="mdc-list-item" role="option" tabindex="0">
-              Vegetables
-            </li>
-            <li class="mdc-list-item" role="option" tabindex="0">
-              Fruit
-            </li>
-          </ul>
-        </div>
-      </div>
       </div>
     );
   }
