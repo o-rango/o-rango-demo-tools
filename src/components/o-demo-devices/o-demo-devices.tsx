d963fdcfb12f362f27b5279b141f4cb72524b3d3
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-25 12:43:59 +0100

    feat : devices bootsrap and init setup

---
+++ src/components/o-demo-devices/o-demo-devices.tsx
@@ -0,0 +1,31 @@
+import { Component, Listen } from '@stencil/core';
+
+@Component({
+  tag: 'o-demo-devices',
+  styleUrl: 'o-demo-devices.scss',
+  shadow: false
+})
+export class DemoDevicesComponent {
+  render() {
+    return (
+      <div class="marvel-device iphone-x">
+        <div class="notch">
+          <div class="camera" />
+          <div class="speaker" />
+        </div>
+        <div class="top-bar" />
+        <div class="sleep" />
+        <div class="bottom-bar" />
+        <div class="volume" />
+        <div class="overflow">
+          <div class="shadow shadow--tr" />
+          <div class="shadow shadow--tl" />
+          <div class="shadow shadow--br" />
+          <div class="shadow shadow--bl" />
+        </div>
+        <div class="inner-shadow" />
+        <div class="screen">Contents</div>
+      </div>
+    );
+  }
+}
