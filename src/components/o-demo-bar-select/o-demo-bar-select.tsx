2cfaa6ff88156947fb5f1db1a174eb8d4e73b55d
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-25 02:44:33 +0100

    fix() : fix pre-select option

--- src/components/o-demo-bar-select/o-demo-bar-select.tsx
+++ src/components/o-demo-bar-select/o-demo-bar-select.tsx
@@ -3,24 +3,22 @@ import { MDCSelect } from '@material/select';
 @Component({
   tag: 'o-demo-bar-select',
   styleUrl: 'o-demo-bar-select.scss',
-  shadow: false
+  shadow: true
 })
 export class DemoSelectComponent {
   private select: any;
   @Element() el: HTMLElement;
   @Event() selectedCaseChanged: EventEmitter;
-  selectedCaseChangedHandler(payload: any) {
-    this.selectedCaseChanged.emit(payload);
-  }
   @Prop() options: any;
 
   componentDidLoad() {
-    const rootEl = this.el.querySelector('.mdc-select');
+    const rootEl = this.el.shadowRoot.querySelector('.mdc-select');
     this.select = new MDCSelect(rootEl);
-    this.select.listen('MDCSelect:change', evt => {
-      this.selectedCaseChangedHandler(evt.detail.value);
-    });
     this.select.selectedIndex = 0;
+
+    this.select.listen('MDCSelect:change', evt => {
+      this.selectedCaseChanged.emit(evt.detail.value);
+     });
   }
   componentDidUnload() {
     this.select.destroy();
@@ -28,10 +26,10 @@ export class DemoSelectComponent {
 
   render() {
     return (
-      <div class="my-select-container">
+      <div>
         <div class="mdc-select" role="listbox">
           <div class="mdc-select__surface" tabindex="0">
-            <div class="mdc-select__label">Demo case</div>
+            <div class="mdc-select__label  mdc-select__label--float-above">Demo case</div>
             <div class="mdc-select__selected-text" />
             <div class="mdc-select__bottom-line" />
           </div>
