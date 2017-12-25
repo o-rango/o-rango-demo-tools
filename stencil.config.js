328ccd8fce057e8cba933a4e620edce29f8a9183
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-25 03:36:48 +0100

    fix(git) amend repository and namespace props

--- stencil.config.js
+++ stencil.config.js
@@ -1,9 +1,9 @@
 exports.config = {
-  namespace: 'orango-dev-tools',
+  namespace: 'orango-demo-tools',
   generateDistribution: false,
   bundles: [
     { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select' , 'o-demo-bar-buttons' , 'o-demo-snackbar'] },
-    { components: ['o-demo-case'] }
+    { components: ['o-demo-case' , 'o-demo-devices'] }
   ],
   collections: [],
   sassConfig: {
