3d24382ef6bfd6adb6ac80db758aabf48278562d
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-25 02:45:16 +0100

    chore(stencil) : added new components

--- stencil.config.js
+++ stencil.config.js
@@ -2,7 +2,7 @@ exports.config = {
   namespace: 'orango-dev-tools',
   generateDistribution: false,
   bundles: [
-    { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select' , 'o-demo-bar-buttons'] },
+    { components: ['o-demo-bar' , 'o-demo-bar-toolbar' , 'o-demo-bar-select' , 'o-demo-bar-buttons' , 'o-demo-snackbar'] },
     { components: ['o-demo-case'] }
   ],
   collections: [],
