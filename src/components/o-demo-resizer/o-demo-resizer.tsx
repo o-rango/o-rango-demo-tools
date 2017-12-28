ec0d144b687f948bae7edcf3d334e9fecead81bd
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-12-28 15:58:00 +0100

    feat(component) add initial resizer component features

--- src/components/o-demo-resizer/o-demo-resizer.tsx
+++ src/components/o-demo-resizer/o-demo-resizer.tsx
@@ -1,4 +1,4 @@
-import { Component, Listen } from '@stencil/core';
+import { Component, Event, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'o-demo-resizer',
@@ -6,9 +6,45 @@ import { Component, Listen } from '@stencil/core';
   shadow: true
 })
 export class DemoResizerComponent {
+  private viewports: any[] = [
+    { size: '1600', name: 'Window xlarge' },
+    { size: '1440', name: 'Window xlarge' },
+    { size: '1280', name: 'Window large' },
+    { size: '900', name: 'Window medium' },
+    { size: '840', name: 'Window medium' },
+    { size: '600', name: 'Window small' },
+    { size: '480', name: 'Window xsmall' }
+  ];
+
+  @Event() resizeButtonClicked: EventEmitter;
+  handleClick(event: any) {
+    let evt = event.currentTarget.getAttribute('data-size');
+    this.resizeButtonClicked.emit(evt);
+  }
+
   render() {
     return (
-        <div>cASDADAS</div>
+      <div class="resize-toolbar-container">
+        <div class="resize-toolbar">
+          {this.viewports.map(option => {
+            var cssSize = {
+              width: `${option.size}px`
+            };
+            return (
+              <div
+                onClick={(event: UIEvent) => this.handleClick(event)}
+                class="item-resize-toolbar"
+                style={cssSize}
+                data-name={option.size}
+                data-size={option.size}
+              >
+                <div class="left device-resizer">{option.size}</div>
+                <div class="rigth device-resizer">{option.size}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
