c7e6d01b553f99b16c51ffc3dcba1c82403c8cdd
roca <romulo.cintra@gft.com>
2018-04-05 11:02:54 +0200

    feat(o-demo-bar-toolbar): update toolbar

--- src/components/o-demo-bar-toolbar/o-demo-bar-toolbar.tsx
+++ src/components/o-demo-bar-toolbar/o-demo-bar-toolbar.tsx
@@ -4,18 +4,20 @@ import { MDCToolbar } from '@material/toolbar';
 @Component({
   tag: 'o-demo-bar-toolbar',
   styleUrl: 'o-demo-bar-toolbar.scss',
-  shadow: false
+  shadow: true
 })
 export class DemoToolbarComponent {
-  private toolbar: any;
+
   @Element() el: HTMLElement;
   @Prop() name: string;
   @Prop() options: any;
+  toolbar: any;
+  rootEl: any;
 
   componentDidLoad() {
-    const rootEl = this.el.querySelector('.mdc-toolbar');
-    this.toolbar = new MDCToolbar(rootEl);
-    this.toolbar.fixedAdjustElement = this.el.querySelector(
+    this.rootEl = this.el.shadowRoot.querySelector('.mdc-toolbar');
+    this.toolbar = new MDCToolbar(this.rootEl);
+    this.toolbar.fixedAdjustElement = this.el.shadowRoot.querySelector(
       '.mdc-toolbar-fixed-adjust'
     );
   }
@@ -29,14 +31,15 @@ export class DemoToolbarComponent {
       <div class="mdc-typography">
         <header class="mdc-toolbar mdc-toolbar--fixed">
           <div class="mdc-toolbar__row">
-            <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+            <section id="left-panel" class="mdc-toolbar__section mdc-toolbar__section--align-start">
               <h3 class="mdc-typography--subheading2">{this.name}</h3>
               <slot name="left" />
             </section>
-            <section class="mdc-toolbar__section">
+            <section id="center-panel" class="mdc-toolbar__section">
               <slot name="center" />
             </section>
             <section
+              id="right-panel"
               class="mdc-toolbar__section mdc-toolbar__section--align-end"
               role="toolbar"
             >
