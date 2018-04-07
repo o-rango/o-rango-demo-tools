b72215b066c8d9f368eec4e6663d58ebe54960ba
roca <romulo.cintra@gft.com>
2018-04-07 12:31:09 +0200

    demo  update

--- docs/build/orango-demo-tools/o-demo-case.js
+++ docs/build/orango-demo-tools/o-demo-case.js
@@ -2,13 +2,4 @@
  * O-RANGO - MIT License
  * Built with http://stenciljs.com
  */
-const { h } = window.OrangoDemoTools;
-
-class DemoCaseComponent {
-    static get is() { return "o-demo-case"; }
-    static get encapsulation() { return "shadow"; }
-    static get properties() { return { "name": { "type": String, "attr": "name" }, "scripts": { "type": String, "attr": "scripts" } }; }
-    static get style() { return ""; }
-}
-
-export { DemoCaseComponent as ODemoCase };
+const{h:t}=window.OrangoDemoTools;class e{static get is(){return"o-demo-case"}static get encapsulation(){return"shadow"}static get properties(){return{name:{type:String,attr:"name"},scripts:{type:String,attr:"scripts"}}}static get style(){return""}}export{e as ODemoCase};
