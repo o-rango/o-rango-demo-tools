81daa7d5a54f98e4c7e570363f351ac3e01e0fd8
roca <romulo.cintra@gft.com>
2018-04-07 21:14:07 +0200

    update builds

---
+++ docs/build/orango-demo-tools/ovmkfnp3.es5.js
@@ -0,0 +1,5 @@
+/*!
+ * O-RANGO - MIT License
+ * Built with http://stenciljs.com
+ */
+OrangoDemoTools.loadBundle("ovmkfnp3",["exports"],function(e){window.OrangoDemoTools.h;var n=function(){function e(){}return Object.defineProperty(e,"is",{get:function(){return"o-demo-case"},enumerable:!0,configurable:!0}),Object.defineProperty(e,"encapsulation",{get:function(){return"shadow"},enumerable:!0,configurable:!0}),Object.defineProperty(e,"properties",{get:function(){return{name:{type:String,attr:"name"},scripts:{type:String,attr:"scripts"}}},enumerable:!0,configurable:!0}),Object.defineProperty(e,"style",{get:function(){return""},enumerable:!0,configurable:!0}),e}();e.ODemoCase=n,Object.defineProperty(e,"__esModule",{value:!0})});
