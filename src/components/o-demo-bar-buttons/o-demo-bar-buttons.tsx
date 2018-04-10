4233c51fdca93233a50ab9b25a4cae089e618ab4
roca <romulo.cintra@gft.com>
2018-04-10 15:50:24 +0200

    prepare for active buttons

--- src/components/o-demo-bar-buttons/o-demo-bar-buttons.tsx
+++ src/components/o-demo-bar-buttons/o-demo-bar-buttons.tsx
@@ -15,18 +15,20 @@ export class DemoButtonsComponent {
   render() {
     return (
       <div class="toolbar-icons">
-        <svg
+
+     <button class="toolbar-button" >
+     <svg
          data-btn="other-devices"
          onClick={(event: UIEvent) => this.handleClick(event)}
-         height="26"
+         height="24"
          viewBox="0 0 24 24"
-         width="26"
+         width="24"
          xmlns="http://www.w3.org/2000/svg">
              <path d="M0 0h24v24H0z" fill="none"/>
              <path d="M3 6h18V4H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h4v-2H3V6zm10 6H9v1.78c-.61.55-1 1.33-1 2.22s.39 1.67 1 2.22V20h4v-1.78c.61-.55 1-1.34 1-2.22s-.39-1.67-1-2.22V12zm-2 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM22 8h-6c-.5 0-1 .5-1 1v10c0 .5.5 1 1 1h6c.5 0 1-.5 1-1V9c0-.5-.5-1-1-1zm-1 10h-4v-8h4v8z"/>
-      </svg>
-
-
+       </svg>
+     </button>
+     <button class="toolbar-button" >
         <svg
           data-btn="mobile"
           onClick={(event: UIEvent) => this.handleClick(event)}
@@ -38,7 +40,8 @@ export class DemoButtonsComponent {
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z" />
         </svg>
-
+        </button>
+        <button class="toolbar-button" >
         <svg
           data-btn="desktop"
           onClick={(event: UIEvent) => this.handleClick(event)}
@@ -50,7 +53,8 @@ export class DemoButtonsComponent {
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7l-2 3v1h8v-1l-2-3h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 12H3V4h18v10z" />
         </svg>
-
+        </button>
+        <button class="toolbar-button" >
         <svg
           data-btn="grid-pattern"
           onClick={(event: UIEvent) => this.handleClick(event)}
@@ -63,6 +67,7 @@ export class DemoButtonsComponent {
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M8 4v1.45l2 2V4h4v4h-3.45l2 2H14v1.45l2 2V10h4v4h-3.45l2 2H20v1.45l2 2V4c0-1.1-.9-2-2-2H4.55l2 2H8zm8 0h4v4h-4V4zM1.27 1.27L0 2.55l2 2V20c0 1.1.9 2 2 2h15.46l2 2 1.27-1.27L1.27 1.27zM10 12.55L11.45 14H10v-1.45zm-6-6L5.45 8H4V6.55zM8 20H4v-4h4v4zm0-6H4v-4h3.45l.55.55V14zm6 6h-4v-4h3.45l.55.54V20zm2 0v-1.46L17.46 20H16z" />
         </svg>
+        </button>
       </div>
     );
   }
