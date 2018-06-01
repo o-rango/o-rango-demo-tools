e81d8c4e1cf984af536c6cc15ae3596615b8ad1a
roca <romulo.cintra@gft.com>
2018-06-01 12:20:01 +0200

    docs

--- docs/build/orango-demo-tools/6nzshyjy.js
+++
@@ -1,5 +0,0 @@
-/*!
- * O-RANGO - MIT License
- * Built with http://stenciljs.com
- */
-const{h:e}=window.OrangoDemoTools;class t{static get is(){return"o-demo-case"}static get encapsulation(){return"shadow"}static get properties(){return{name:{type:String,attr:"name"}}}static get style(){return""}}export{t as ODemoCase};
