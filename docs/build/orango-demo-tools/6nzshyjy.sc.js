01326e348d6d7e1465444c3dc2811168955dd2c0
roca <romulo.cintra@gft.com>
2018-06-01 12:22:01 +0200

    test:docs --prod

---
+++ docs/build/orango-demo-tools/6nzshyjy.sc.js
@@ -0,0 +1,5 @@
+/*!
+ * O-RANGO - MIT License
+ * Built with http://stenciljs.com
+ */
+const{h:e}=window.OrangoDemoTools;class t{static get is(){return"o-demo-case"}static get encapsulation(){return"shadow"}static get properties(){return{name:{type:String,attr:"name"}}}static get style(){return""}}export{t as ODemoCase};
